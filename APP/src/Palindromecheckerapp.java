3570db7ee62ca460cdf508c8aa9df6eda01ae465
aditya10maurya <turkey2569@gmail.com>
2026-03-17 15:10:42 +0530

    UC2

--- APP/src/Palindromecheckerapp.java
+++ APP/src/Palindromecheckerapp.java
@@ -1,5 +1,22 @@
 public class Palindromecheckerapp {
-    public static void main(String[] args){
-        System.out.println("Welcome to Palendrome Checker App Management System");
+
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String input = "madam";
+
+        // Reverse the string
+        String reversed = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+
+        // Check if palindrome
+        if (input.equals(reversed)) {
+            System.out.println("The string \"" + input + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is NOT a Palindrome.");
+        }
     }
 }
